064c16c06c1778d7dbdb717d7d697f0bce83dde9
Nishiyama-i3 <nishiyama@i3-systems.com>
2021-03-16 14:16:28 +0900

    MainActivityにログの出力を追加

--- app/src/main/java/com/example/sampleapp/MainActivity.kt
+++ app/src/main/java/com/example/sampleapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.sampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import android.content.ContentValues.TAG
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            // Log and toast
+            val msg = token!!
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
     }
 }
